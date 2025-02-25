b8de5849da97cace471ddbb0b1685bf4b7143148
Eloi RM <eloimailmaynad@gmail.com>
2025-02-25 11:16:18 +0100

    FF

--- src/poke/Main.java
+++ src/poke/Main.java
@@ -16,6 +16,25 @@ import org.json.simple.parser.ParseException;
 
 public class Main {
 
+    public static JSONArray getPokemonJson() throws Exception {
+        try {
+            FileReader reader = new FileReader("src\\pokedex.json");
+            
+            JSONParser parser = new JSONParser();
+            JSONObject jsonObject = (JSONObject) parser.parse(reader);
+
+            JSONArray jsonArray = (JSONArray) jsonObject.get("pokemon");
+
+            reader.close();
+            
+            return jsonArray;
+ 
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+            return new JSONArray();
+        }  
+    }
+
     public static void numberOfPokemon(JSONArray jsonArray) {
         System.out.println(jsonArray.size());
     }
@@ -44,9 +63,10 @@ public class Main {
         
         Collections.sort(pokeList, new Comparator<JSONObject>() {
             public int compare(JSONObject a, JSONObject b) {
-                // Convert from String to Double for correct comparison
+
                 double weightA = StringToDouble((String) a.get("weight"));
                 double weightB = StringToDouble((String) b.get("weight"));
+
                 return Double.compare(weightA, weightB);
             }
         });
@@ -65,27 +85,77 @@ public class Main {
         return doubleString;
     }
 
-    public static void main(String[] args) throws Exception {
-        try {
-            // Read file
-            FileReader reader = new FileReader("src\\pokedex.json");
-            
-            // Parse JSON file
-            JSONParser parser = new JSONParser();
-            JSONObject jsonObject = (JSONObject) parser.parse(reader);
+    public static JSONObject getPokemon(String pokemonName) throws Exception {
+        JSONArray jsonArray = getPokemonJson();
 
-            JSONArray jsonArray = (JSONArray) jsonObject.get("pokemon");
+        JSONObject desiredPoke = new JSONObject();
 
-            // Process the JSON data
-            // ...
-            numberOfPokemon(jsonArray);
-            pokemonWithMin10Kg(jsonArray);
-            pokemonOrderedByWeight(jsonArray);
-            
-            // Close reader
-            reader.close();
-        } catch (IOException | ParseException e) {
-            e.printStackTrace();
+        for (Object obj : jsonArray) {
+            JSONObject pokemon = (JSONObject) obj;
+
+            if (pokemon.containsKey("name") && pokemon.get("name").equals(pokemonName)) {
+                desiredPoke = pokemon;
+            }
         }
+
+        return desiredPoke;
+    }
+    
+    public static void pokemonEvolutiontree(String pokemonName) throws Exception {
+        String EvolutionTree = pokemonName;
+
+        JSONObject desiredPoke = getPokemon(pokemonName);
+
+        String pokeName = (String) desiredPoke.get("name");
+
+        if (desiredPoke.containsKey("prev_evolution")) {
+            
+            JSONArray prevEvoCateg = (JSONArray) desiredPoke.get("prev_evolution");
+            JSONObject prevEvo = (JSONObject) prevEvoCateg.get(0);
+            String prevEvoName = (String) prevEvo.get("name");
+            
+            EvolutionTree = prevEvoName  + " -> " + pokeName;
+
+            JSONObject prePrevPoke = getPokemon(prevEvoName);
+            
+            if (prePrevPoke.containsKey("prev_evolution")) {
+                JSONArray prePrevEvoCateg = (JSONArray) prePrevPoke.get("prev_evolution");
+                JSONObject prePrevEvo = (JSONObject) prePrevEvoCateg.get(0);
+                String prePrevEvoName = (String) prePrevEvo.get("name");
+                
+                EvolutionTree = prePrevEvoName + " -> " + prevEvoName  + " -> " + pokeName;
+            }
+        }
+
+        if (desiredPoke.containsKey("next_evolution")) {
+            
+            JSONArray nextEvoCateg = (JSONArray) desiredPoke.get("next_evolution");
+            JSONObject nextEvo = (JSONObject) nextEvoCateg.get(0);
+            String nextEvoName = (String) nextEvo.get("name");
+            
+            EvolutionTree += " -> " + nextEvoName;
+
+            JSONObject nextNextPoke = getPokemon(nextEvoName);
+            
+            if (nextNextPoke.containsKey("prev_evolution")) {
+                JSONArray nextNextEvoCate = (JSONArray) nextNextPoke.get("prev_evolution");
+                JSONObject nextNextEvo = (JSONObject) nextNextEvoCate.get(0);
+                String nextNextEvoName = (String) nextNextEvo.get("name");
+                
+                EvolutionTree =  pokeName + " -> " + nextEvoName  + " -> " +  nextNextEvoName;
+            }
+        }
+
+        System.out.println(EvolutionTree);
+    }
+    public static void main(String[] args) throws Exception {
+
+        JSONArray jsonArray = getPokemonJson();
+
+        // numberOfPokemon(jsonArray);
+        // pokemonWithMin10Kg(jsonArray);
+        // pokemonOrderedByWeight(jsonArray);
+        pokemonEvolutiontree("Ivysaur");
+             
     }
 }
